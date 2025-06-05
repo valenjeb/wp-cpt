5249f72b63122c5cbe3baf3c0bb9167667c347c9
Valentin Jebelev <7081274+valenjeb@users.noreply.github.com>
2025-06-05 10:57:42 +0300

    Update the plugin version to 1.1.0

--- wp-cpt.php
+++ wp-cpt.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Plugin Name:     WP Custom Post Types & Taxonomies
- * Plugin URI:      https://github.com/valenjeb/wp-cpt
- * Description:     A helper library for creating WordPress custom post types an taxonomies in a OOP way.
- * Author:          Valentin Jebelev
- * Author URI:      https://github.com/valenjeb
- * Text Domain:     wp-cpt
- * Domain Path:     /languages
- * Version:         0.1.0
+ * Plugin Name: WP Custom Post Types & Taxonomies
+ * Plugin URI:  https://github.com/valenjeb/wp-cpt
+ * Description: A helper library for creating WordPress custom post types an taxonomies in a OOP way.
+ * Author:      Valentin Jebelev
+ * Author URI:  https://github.com/valenjeb
+ * Text Domain: wp-cpt
+ * Domain Path: /languages
+ * Version:     1.1.0
  */
 
 declare(strict_types=1);
